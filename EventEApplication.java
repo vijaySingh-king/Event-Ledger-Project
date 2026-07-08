80febf6ba7aa608cccd287c3af1eff5b9c4c9bcc
Vijaysingh <53387123+vijaySingh-king@users.noreply.github.com>
2026-07-09 01:09:46 +0530

    Add files via upload
    Main Class

---
+++ EventEApplication.java
@@ -0,0 +1,13 @@
+package com.example.event_ledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventEApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventEApplication.class, args);
+	}
+
+}
